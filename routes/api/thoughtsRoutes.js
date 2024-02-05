073bf7212bfdb6670ddce2502b932c37c30e9356
Hannahphoto <hannahprice67@gmail.com>
2024-02-04 23:44:55 -0500

    need to do seeds and get server connected

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const{
+    getThoughts,
+    getSingleThought,
+    createThought,
+    deleteThought,
+} = require('../../controllers/thouhghtsController');
+
+////api/users
+router.route('/').get(getThoughts).post(createThought);
+
+//api/users/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const {Thoughts} = require('./models');
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
-//Get all users
+// ???//Get all users
 
 //Get single user by _id
 
@@ -20,7 +20,7 @@ app.use(express.json());
 
 //put - update user by _id
 
-//delete - remove a user by _id
+// delete - remove a user by _id???????
 
 db.once('open', ()=>{
     app.listen(PORT, ()=>{
